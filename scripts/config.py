0fc76ab368780b8be2c9c487702eda81c77549d0
atticuseby <atticus@banditmediagroup.com>
2025-05-25 00:35:46 -0500

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,8 @@
+# config.py
+
 import os
 
-OPTIMIZELY_API_TOKEN = os.getenv("OPTIMIZELY_API_TOKEN")
-RICS_API_TOKEN = os.getenv("RICS_API_TOKEN")
-DRY_RUN = os.getenv("DRY_RUN", "true").lower() == "true"
+OPTIMIZELY_API_TOKEN = os.getenv("OPTIMIZELY_API_TOKEN", "")
+RICS_API_TOKEN = os.getenv("RICS_API_TOKEN", "")
+DRY_RUN = os.getenv("DRY_RUN", "True").lower() == "true"
+TEST_EMAIL = os.getenv("TEST_EMAIL", "test_email@yourdomain.com")

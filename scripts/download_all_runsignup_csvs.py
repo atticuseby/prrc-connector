02a177550e719971bd76508fcfc8f511fe09c3a8
atticuseby <atticus@banditmediagroup.com>
2025-05-27 11:35:46 -0500

    Update download_all_runsignup_csvs.py

--- scripts/download_all_runsignup_csvs.py
+++ scripts/download_all_runsignup_csvs.py
@@ -1,6 +1,7 @@
-# scripts/download_all_runsignup_csvs.py
-
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import time
 import json
 from datetime import datetime
